34dc3b5d03ea04df089cb09a7d218fc5d9b2775c
Neobraingit <Neobraingit@github.com>
2022-10-25 20:08:36 +0200

    Comentarios

---
+++ Comentarios.py
@@ -0,0 +1,6 @@
+# Los comentarios permiten insertar texto y anotaciones sin que intervenga en el código y se hacen con #
+# Esto es un comentario
+
+# Comentarios de varias líneas
+'''Esto es un comentario de varias líneas
+y lo hacemos con la triple comilla'''
