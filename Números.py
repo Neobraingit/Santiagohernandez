a4445d686db5341dc431b9739f9fc3394fc398a7
Neobraingit <Neobraingit@github.com>
2022-10-25 20:05:39 +0200

    Números

---
+++ Números.py
@@ -0,0 +1,28 @@
+# En Python existen dos tipos de datos numèricos: int y float
+numeroentero = 2 # Esto es un número entero
+float = 2.2 # Esto es un float
+print (numeroentero)
+print (float)
+
+# Casting o trasformación es convertir un string a un número
+num = '10' # Esto es un string
+num2= int(num) # Casteamos el string a un número entero
+print (num2)
+
+# Podemos definir un nùmero tan grande como soporte la memoria de mi ordenador
+
+# Para una mejor comprensión del código se hace con guiones bajos
+num3 = 1_000_000
+print (num3)
+
+# Los número flotantes si tienen límite
+num4 = 2e400
+print  (num4) # Esto nos devuelve un inf que significa infinito
+
+
+
+
+
+
+
+
