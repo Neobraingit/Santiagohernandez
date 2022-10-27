c63adce6dbc49160a2ec00bef27c6e044e9bd702
Neobraingit <Neobraingit@github.com>
2022-10-27 18:33:56 +0200

    Operadores lógicos

---
+++ Operadores lógicos.py
@@ -0,0 +1,19 @@
+# ¿Cuáles son los operadores lógicos?
+''' not
+    or
+    and'''
+
+# Not
+num = 5
+num < 10
+print (not num < 10)
+
+# Or
+num1 = 5
+num2 = 10
+print ((num1 > 1) or (num1 == 5))
+
+# And
+num3 = 10
+num4 = 5
+print ((num3 > num4) and (num3 < num4))
