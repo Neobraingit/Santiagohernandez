7f7c3c8c5faa4b406343a2158910559d99fc4e04
Neobraingit <Neobraingit@github.com>
2022-10-27 17:50:21 +0200

    Operadores de asignación

---
+++ Operadores de asignación.py
@@ -0,0 +1,18 @@
+# ¿Cuáles son los operadores de asignación?
+''' a += 5  es equivalente a a = a +5
+     a /= es equivalente a a = a / 10
+     a *= b es equivalente a a = a * b'''
+
+num = 100
+num *= 2
+print (num)
+
+num += 100
+print (num)
+
+num /= 100
+print (num)
+
+
+
+
