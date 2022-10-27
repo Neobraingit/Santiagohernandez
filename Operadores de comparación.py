28df792e336d31eab0706aaaeedb8f2512e22cea
Neobraingit <Neobraingit@github.com>
2022-10-27 18:07:49 +0200

    Operadores de comparación

---
+++ Operadores de comparación.py
@@ -0,0 +1,23 @@
+# ¿Cuáles son los operadores de comparación?
+''' == igual a
+    ¡= diferente que
+    < menor que
+    > mayor que
+    <= menor o igual que
+    >= mayor o igual que'''
+
+print (2 == 2)
+print (2 != 1)
+print (2 > 3)
+print (2 < 3)
+print (2 >= 3)
+print (2 <= 3)
+
+# Strings
+texto = 'cadena de texto'
+texto2 = 'cadena de texto2'
+print (texto == 'cadena de texto')
+
+
+
+
