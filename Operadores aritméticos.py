953a4de960b2d9e2bc807418f7fb76d883644252
Neobraingit <Neobraingit@github.com>
2022-10-27 17:41:16 +0200

    Operadores aritméticos

---
+++ Operadores aritméticos.py
@@ -0,0 +1,37 @@
+# ¿Qué son los operadores aritméticos?
+# +, -, *, /, **
+
+# Suma y resta
+num = 10
+num2 = 20
+print (num +num2)
+print (num - num2)
+
+# Multiplicación y división
+num3 = 5
+num4 = 6
+print (num3 * num4)
+print (num3 / num4)
+
+# Strings con operadores aritméticos (concatenación). Solo podemos utilizar el operador suma y multiplicación
+string1 = 'Hola mi nombre es '
+string2 = 'Marcos Carmona García'
+print (string1 + string2)
+print (string1 * 5 + string2)
+
+# Módulo
+num5 = 30
+num6 = 10
+print (num5 % num6)
+
+# Exponencial
+num7 = 38
+num8 = 38
+print (num7 ** 2)
+print (num8 ** 4)
+
+# Floor division
+num9 = 34
+num10 = 23
+print (num9 // num10)
+
