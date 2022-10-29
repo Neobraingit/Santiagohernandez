d0e34fce4140a224dba89b9ae7d8995bb898d9be
Neobraingit <Neobraingit@github.com>
2022-10-29 12:45:54 +0200

    Bytes y Bytesarray

---
+++ Bytes y Bytearray.py
@@ -0,0 +1,10 @@
+# ¿Qué son los bytes y bytarray?
+'''Un byte es una ubicación de menmoria con un tamalo de 8 bits, inmutable y parecido a un string'''
+'''La sintaxix para representarlos son b'<cadena de bytes>'''
+cadena_bytes = b'\x02\x1f' # Esto es hexadecimal y corresponde al número entero 543
+print (cadena_bytes)
+print (bin(543)) # Conocemos el número en binario de 543
+print (hex(543)) # Transformamos el número en hexadecimal
+
+
+
