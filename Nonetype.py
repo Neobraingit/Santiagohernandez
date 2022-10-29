087900a43312f1daa0754563feba50ab563c410d
Neobraingit <Neobraingit@github.com>
2022-10-29 13:05:03 +0200

    Nonetype

---
+++ Nonetype.py
@@ -0,0 +1,15 @@
+# ¿Qué es el tipo de dato Nonetype?
+'''La palabra none se utiuliza para definir una variable nula o un objeto vacío'''
+def mifuncion():
+    return    # Esta función que no devuelve nada es none
+
+mifuncion()
+
+var = None
+print (var)
+
+'''None no es lo mismo que false
+   None no es 0
+   None no es un string vacío
+   Comparar None con cualquier cosa te devolverá siempre False menos con None'''
+
