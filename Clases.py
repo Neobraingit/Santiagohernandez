91899b8ff7132debce4a49c94368f612e665dfa3
Neobraingit <Neobraingit@github.com>
2022-10-30 16:10:14 +0100

    Clases

---
+++ Clases.py
@@ -0,0 +1,15 @@
+# ¿Qué es la Orientación a Objetos en Python?
+'''Python es un lenguaje orientado a objetos y todas las estructuras que hemos visto son objetos'''
+
+# ¿Qué son las clases?
+'''Las clases son la estructura principal de la programación orientada a objetos y nos proporciona
+un medio para agrupar datos y funcionalidades'''
+class Coche:
+    pass
+
+# ¿Qué son los objetos?
+'''Son instancias de nuestra clase'''
+coche1 = Coche() # Creamos una instancia a partir de nuestra clase y se llama objeto
+print (coche1)
+coche2 = Coche()
+print (coche2)
