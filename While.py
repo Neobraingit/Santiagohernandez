f827d9f44f5ea726fae7b5084424955e4e81656e
Neobraingit <Neobraingit@github.com>
2022-10-30 08:42:45 +0100

    While

---
+++ While.py
@@ -0,0 +1,23 @@
+# ¿Què es la sentencia While?
+'''Nos permite repetir una setencia un número infinito de veces. La sintaxis es:
+while expresión
+     sentencia'''
+num = 10
+while num > 0:
+    num -= 1
+    print (num)
+
+# Claúsula else
+num = 5
+while num > 0:
+    num -= 1
+    print (num)
+else:
+    print ('Sentencia final del bucle')
+
+# Bucles infinitos
+num = 5
+while num > 0:
+    print ('Esto es infinito')
+
+
