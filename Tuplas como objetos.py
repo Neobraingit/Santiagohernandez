89bcdc3a3a2f975b5c4ab33cb3880e8f7d03f97c
Neobraingit <Neobraingit@github.com>
2022-11-01 12:45:50 +0100

    Tuplas como objetos

---
+++ Tuplas como objetos.py
@@ -0,0 +1,3 @@
+tupla = ('Perro', 'Gatos', 'Pájaro')
+print (tupla.index('Perro')) # Nos muestra el índice de un elemento
+print (tupla.count('Pájaro')) # NOs muestra el número de veces que aparece un elemento
